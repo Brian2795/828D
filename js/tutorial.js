aab142dc3e7e0adbd14a6ac0575cd5ce507352be
Anthony Li <antli.inbox@gmail.com>
2018-12-04 21:58:29 -0500

    Add base methods for the tutorial page

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,13 +1,54 @@
 var tutorialState = {
+	preload: function() {
+		console.log("Running tutorial preload");
+		game.load.image('sampleimage', 'assets/images/sample.png');
+	},
 
 	create: function(){
 		console.log("Loading the tutorial");
 		this.initBackground();
+		this.addHeader("Some header", 300, 300);
+		this.addBackButton();
 	},
 
 	initBackground: function() {
 		var backgroundImage = game.add.sprite(0, 0, 'menu-bg');
 		backgroundImage.width = game.width;
 		backgroundImage.height = game.height;
-	}
+	},
+
+	addHeader: function(text, xPos, yPos) {
+		game.add.text(xPos, yPos, text, style.navitem.default);
+	},
+
+	addImage: function(imagename, xPos, yPos) {
+		game.add.sprite(xPos, yPos, imagename);
+	},
+
+	addTextBody: function(text, xPos, yPos) {
+		game.add.text(xPos, yPos, text, style.basiclabel.default);	
+	},
+
+	addBackButton: function() {
+		var backText = game.add.text(20, 20, "Back", style.navitem.default);
+		var callback = function() { game.state.start('menu'); };
+		this.createTextButton(backText, "Back", "Back", callback);
+	},
+
+	createTextButton: function( text, originalString, hoverString, callback ) {
+		text.inputEnabled = true;
+		
+		text.events.onInputOver.add(function (target) {
+		  target.setStyle(style.navitem.hover);
+		  target.setText(hoverString);
+		});
+
+		text.events.onInputOut.add(function (target) {
+		  target.setStyle(style.navitem.default);
+		  target.setText(originalString);
+		});
+
+		text.events.onInputUp.add(callback);
+	},
+
 };
